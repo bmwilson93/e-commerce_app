2005b023dde455841f3860f5fa67a507f582cbb6
Brian Wilson <bmwilson93@gmail.com>
2022-02-08 13:20:12 -0700

    improved the imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import Product from './components/Products/Products';
+
+//import Product from './components/Products/Products';
+//import Navbar from './components/Navbar/Navbar';
+
+import { Products, Navbar } from './components'; // get the components from the components exported in index.js
 
 const App = () => {
   return (
     <div>
-        <Product />
+        <Navbar />
+        <Products />
     </div>
   )
 };
