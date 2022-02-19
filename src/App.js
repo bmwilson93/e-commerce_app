51ad46b2998e95628efe66a721a12f6ea6b7a0c9
Brian Wilson <bmwilson93@gmail.com>
2022-02-19 12:02:22 -0700

    Fetch products list from commerce

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 
 //import Product from './components/Products/Products';
 //import Navbar from './components/Navbar/Navbar';
@@ -6,6 +7,22 @@ import React from 'react';
 import { Products, Navbar } from './components'; // get the components from the components exported in index.js
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  // returns a promise for the list of products from commerce
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list(); 
+
+    setProducts(data);
+  }
+
+  // runs on load to fetch the products from commerce api
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
         <Navbar />
