c60513f06d95e9d37580c39719cd4d7d6d368065
Brian Wilson <bmwilson93@gmail.com>
2022-02-25 11:18:16 -0700

    Added UpdateCart RemoveFromCart and EmptyCart functions

--- src/App.js
+++ src/App.js
@@ -19,8 +19,25 @@ const App = () => {
   }
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
-    setCart(item.cart);
+    const { cart } = await commerce.cart.add(productId, quantity);
+    setCart(cart);
+  }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+
+    setCart(cart);
+  } 
+
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+
+    setCart(cart);
+  }
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
   }
 
   // runs on load to fetch the products from commerce api
@@ -38,7 +55,13 @@ const App = () => {
           <Routes>
 
             <Route path="/" element={<Products products={products} onAddToCart={handleAddToCart}/>} />
-            <Route path="/cart" element={<Cart cart={cart} />} />
+            <Route path="/cart" element={
+              <Cart 
+                cart={cart} 
+                handleUpdateCartQty={handleUpdateCartQty}
+                handleRemoveFromCart={handleRemoveFromCart}
+                handleEmptyCart={handleEmptyCart}
+              />} />
 
           </Routes>
       </div>
