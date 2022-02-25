de5e8e698b4d34985a5ab9bf1b5069aef8d8e914
Brian Wilson <bmwilson93@gmail.com>
2022-02-25 13:04:00 -0700

    Fixed render error by adding render func to controller

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { TextField, Grid } from '@material-ui/core';
-import { useFormContext, Controller, useForm } from 'react-hook-form';
+import { useFormContext, Controller } from 'react-hook-form';
 
 const FormInput = ({ name, label, required }) => {
     const { control } = useFormContext();
     return (
         <Grid item xs={12} sm={6}>
             <Controller 
+
+                
+
                 as={TextField}
                 control={control}
-                fullWidth
-                name={name}
-                label={label}
-                required={required}
+                // fullWidth
+                // name={name}
+                // label={label}
+                // required={required}
+
+                // render={({ field }) => <TextField {...field} />}
+
+                render = {({ field})=> (
+                    <TextField
+                        fullWidth
+                        label={label}
+                        required
+                    />
+                )}
+
             />
         </Grid>
-    )
+    );
 }
 
 export default FormInput;
