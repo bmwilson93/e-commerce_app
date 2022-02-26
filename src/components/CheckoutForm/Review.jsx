6fad6b323543ff941ead71c717dfffa14d1c4b81
Brian Wilson <bmwilson93@gmail.com>
2022-02-26 16:55:57 -0700

    Created Review component

---
+++ src/components/CheckoutForm/Review.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography, List, ListItem, ListItemText } from '@material-ui/core';
+
+const Review = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Review
