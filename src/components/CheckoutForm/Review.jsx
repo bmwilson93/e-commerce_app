f92f08709c77720371d0930f784eb0d0a375cd39
Brian Wilson <bmwilson93@gmail.com>
2022-02-26 17:08:52 -0700

    Fixed spelling mistake

--- src/components/CheckoutForm/Review.jsx
+++ src/components/CheckoutForm/Review.jsx
@@ -5,8 +5,22 @@ const Review = ({ checkoutToken }) => {
     return (
         <>
             <Typography variant="h6" gutterBottom>Order summary</Typography>
+            {/* List of products in the cart */}
             <List disablePadding>
-                
+                {checkoutToken.live.line_items.map((product) => (
+                    <ListItem style={{padding: '10px 0'}} key={product.name}>
+                        <ListItemText primary={product.name} secondary={`Quantity: ${product.quantity}`} />
+                        <Typography variant="body2">{product.line_total.formatted_with_symbol}</Typography>
+                    </ListItem>
+                ))}
+
+                {/* Total */}
+                <ListItem style={{padding: '10px 0'}}>
+                    <ListItemText primary="Total" />
+                    <Typography variant="subtitle1" style={{ fontWeight: 700 }}>
+                        {checkoutToken.live.subtotal.formatted_with_symbol}
+                    </Typography>
+                </ListItem>
             </List>
         </>
     )
